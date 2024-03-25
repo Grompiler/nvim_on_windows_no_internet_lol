12f72cdcc0320b90558c2a2ea5e884d63becccc6
Pierre <32479816+Grompiler@users.noreply.github.com>
2024-03-25 21:02:02 +0100

    Update set.lua

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -34,3 +34,8 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.shell = "powershell.exe"
+vim.opt.shellxquote = ""
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+vim.opt.shellquote = ""
+vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
